1ad44bbed9d094e423f9c5633cb7e30e693ded20
afridi00000 <afridi@usf.edu>
2025-02-12 17:17:14 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,11 @@ function calculateInvoice(subtotal, taxRate, discount) {
 
 console.log(calculateInvoice(100, 0.08, 5)); 
 console.log(calculateInvoice(500, 0.1, 20)); 
+// Task 2: Function Expression - Employee Hourly Wage Calculation
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+};
+
+console.log(calculateHourlyWage(52000, 40)); 
+console.log(calculateHourlyWage(75000, 35)); 
