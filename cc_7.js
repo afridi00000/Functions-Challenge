784b8a85519589a2b87b6ca00bef457bdfe669d5
afridi00000 <afridi@usf.edu>
2025-02-12 17:19:28 -0500

    Task 6 - Filtering High-Value Transactions

--- cc_7.js
+++ cc_7.js
@@ -48,3 +48,10 @@ function calculateLoanInterest(principal, rate, years) {
 
 console.log(calculateLoanInterest(1000, 0.05, 3)); 
 console.log(calculateLoanInterest(5000, 0.07, 5)); 
+// Task 6: Higher-Order Functions - Filtering High-Value Transactions
+function filterHighValueTransactions(transactions, filterFunction) {
+    return transactions.filter(filterFunction);
+}
+
+let transactions = [500, 1200, 3000, 800, 2200];
+console.log(filterHighValueTransactions(transactions, amount => amount > 1000));
